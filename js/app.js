0c079049878375d75d6df64e63b0fd96b13f3a15
bobby@fivemoretalents.com <bobby@fivemoretalents.com>
2020-06-04 21:26:19 -0700

    saving to GH

--- js/app.js
+++ js/app.js
@@ -1,27 +1,45 @@
 'use strict';
 
-
+let globalCache = [];
 
 // Constructor function
 function HornImage(horns) {
-  this.name = horns.name;
   this.keyword = horns.keyword;
   this.description = horns.description;
   this.title = horns.title;
   this.image_url = horns.image_url;
+  globalCache.push(this);
 }
 
 HornImage.prototype.render = function () {
-  let $hornsClone = $('#photo-template').clone();
-  $('main').append($hornsClone);
-  $hornsClone.find('h2').text(this.name);
-  $hornsClone.find('img').attr('src', this.image_url);
-  $hornsClone.find('p').text(this.description);
-  $hornsClone.find('h3').text(this.title);
-  // $hornsClone.removeID('photo-template');
-  // $hornsClone.('id', this.name);
-
+  let $hornsClone = $("#photo-template").clone();
+  $("main").append($hornsClone);
+  $hornsClone.find("img").attr("src", this.image_url);
+  $hornsClone.find("p").text(this.description);
+  $hornsClone.find("h3").text(this.title);
+  // $hornsClone.removeID(‘photo-template’);
+  // $hornsClone.(‘id’, this.name);
 }
+
+// Create function that loops through globalCache & target keywords
+
+  function dropDownRender(horns) {
+    this.keyword = horns.keyword;
+  } 
+  globalCache.forEach(keyword => {
+    let optionTag = `<option value="${keyword}">${keyword}</option>`;
+    $('select').append(optionTag);
+  });
+
+  dropDownRender.prototype.render = function () {
+    let $hornsClone = $("#photo-template").clone();
+    $("header").append(dropDownRender);
+    dropDownRender.find("option").text(this.keyword);
+  }
+
+
+
+// ==============================================================
 
 HornImage.readJson = () => {
   const ajaxSettings = {
@@ -32,6 +50,9 @@ HornImage.readJson = () => {
   $.ajax('data/page-1.json', ajaxSettings).then(data => {
     data.forEach(item => {
       let horns = new HornImage(item);
+      //call new dropdownRender function here
+      dropDownRender();
+      //Call handleDropdown function- use .then's to chain to ajax call
       console.log(horns);
       horns.render();
     });
@@ -39,3 +60,34 @@ HornImage.readJson = () => {
 };
 
 $(() => HornImage.readJson());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // JSONARRAY.forEach creates array of all keywords (HornImage.keyword)
+  // -push to keywordsArray
+  // jsonarray.forEach(function (keyword) =>{
+  //   keywordArray.push(HornImage.keyword);
+  // });
+
+  // keywordArray.forEach //loops over keywordArray
+  //  selects only the specific keywords that user chooses};
